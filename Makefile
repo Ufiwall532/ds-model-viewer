a128ef81b3755815e34419ad1b7240d8220b20f2
deadcast <deadcast@sent.com>
2016-11-06 14:01:49 -0800

    Checking in a really rought collada file parser test

--- Makefile
+++ Makefile
@@ -1,29 +1,29 @@
-NAME = main
-TARGET = $(NAME).nds
+TARGET = main.nds
 CC = $(DEVKITARM)/bin/arm-none-eabi-gcc
 ARCH = -mthumb -mthumb-interwork
 CFLAGS = -g -Wall -O2 -march=armv5te -mtune=arm946e-s -fomit-frame-pointer -ffast-math $(ARCH) -DARM9
 NDSTOOL = $(DEVKITARM)/bin/ndstool
 NDSEMU = /usr/local/bin/desmume-cli
 GAME_ICON = $(DEVKITPRO)/libnds/icon.bmp
-GAME_TITLE = Hello by Caleb
+GAME_TITLE = COLLADA Model Viewer
 
 .PHONY: run clean
 
-$(TARGET): $(NAME).o $(NAME).elf
-	$(NDSTOOL) -c $(TARGET) -9 $(NAME).elf -b $(GAME_ICON) "$(GAME_TITLE)" -d models
+$(TARGET): main.o main.elf
+	$(NDSTOOL) -c $(TARGET) -9 main.elf -b $(GAME_ICON) "$(GAME_TITLE)" -d models
 
-main.o: $(NAME).c
-	$(CC) -MMD -MP -MF $(NAME).d $(CFLAGS) -c $(NAME).c -I$(DEVKITPRO)/libnds/include \
-	-I/usr/local/include/libxml2 -o $(NAME).o
+main.elf: main.o simplexml.o
+	$(CC) -specs=ds_arm9.specs -g $(ARCH) main.o simplexml.o -L$(DEVKITPRO)/libnds/lib -lfilesystem -lfat -lnds9 -o main.elf
 
-main.elf: $(NAME).o
-	$(CC) -specs=ds_arm9.specs -g $(ARCH) $(NAME).o -L$(DEVKITPRO)/libnds/lib -lfilesystem -lfat -lnds9 -o $(NAME).elf
+main.o: main.c
+	$(CC) $(CFLAGS) -c main.c -I$(DEVKITPRO)/libnds/include
+
+simplexml.o: simplexml.c simplexml.h
+	$(CC) $(CFLAGS) -c simplexml.c
 
 run:
 	$(NDSEMU) $(TARGET)
 
 clean:
-	rm *.o *.d *.elf *.nds 
-
+	rm *.o *.elf *.nds 
 
--- main.c
+++ main.c
@@ -3,19 +3,125 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
+#include <sys/stat.h>
+#include "simplexml.h"
+
+void* float_array_content_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if(event == ADD_ATTRIBUTE) {
+		if (strcmp(szAttribute, "id") == 0) {
+			if(strstr(szValue, "mesh-positions-array")) {
+				simpleXmlPushUserData(parser, "mesh-positions-array");
+			}
+		}
+	} else if(event == ADD_CONTENT) {
+		if(strcmp("mesh-positions-array", (char *)simpleXmlPopUserData(parser)) == 0) {
+			iprintf("%s", szValue);
+		}
+	}
+	return NULL;
+}
+
+void* float_array_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "float_array") == 0) {
+			return float_array_content_xml_handler;
+		}
+	}
+	return NULL;
+}
+
+void* source_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "source") == 0) {
+			return float_array_xml_handler;
+		}
+	}
+	return NULL;
+}
+
+void* mesh_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "mesh") == 0) {
+			return source_xml_handler;
+		}
+	}
+	return NULL;
+}
+
+void* geometries_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "geometry") == 0) {
+			return mesh_xml_handler;
+		}
+	}
+	return NULL;
+}
+
+void* library_geometries_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "library_geometries") == 0) {
+			return geometries_xml_handler;
+		}
+	}
+	return NULL;
+}
+
+void* collada_xml_handler(SimpleXmlParser parser, SimpleXmlEvent event,
+	const char* szName, const char* szAttribute, const char* szValue)
+{
+	if (event == ADD_SUBTAG) {
+		if (strcmp(szName, "COLLADA") == 0) {
+			return library_geometries_xml_handler;
+		}
+	}
+	return NULL;
+}
 
 int main(void) {
 	consoleDemoInit();
 
 	if (nitroFSInit(NULL)) {
-		FILE* model_file = fopen("ico.dae","rb");
+		FILE* model_file = fopen("ico.dae","r");
 		if (model_file) {
 			iprintf("File found :)\n");
+			char* file_data;
+			struct stat file_stat;
+			if(stat("ico.dae", &file_stat) != 0) {
+				iprintf("Failed getting info about the file\n");
+			} else {
+				file_data = malloc(file_stat.st_size + 1);
+				if(!file_data) {
+					iprintf("Not enough memory to load file\n");
+				} else {
+					size_t bytes_read = fread(file_data, sizeof(char), file_stat.st_size, model_file);
+					if(bytes_read == file_stat.st_size) {
+						iprintf("Read all %d bytes into memory!\n", bytes_read);
+						SimpleXmlParser parser = simpleXmlCreateParser(file_data, bytes_read);
+						if(parser) {
+							iprintf("Parsed xml file\n");
+							simpleXmlParse(parser, collada_xml_handler);
+							simpleXmlDestroyParser(parser);
+						}
+					}
+				}
+				free(file_data);
+			}
 		} else {
 			iprintf("File not found :(\n");
 		}
+		fclose(model_file);
 	}
 
 	while(1) {
@@ -24,3 +130,4 @@ int main(void) {
 
 	return 0;
 }
+
